242e21552008ae5743ce9ba89b6c0e5f5942a25e
rubenkiworks <rubengomezkiworks@gmail.com>
2024-10-01 17:09:35 +0200

    prueba conflictos

--- GitHubDemo/src/com/example/GitHUbDemoMain.java
+++ GitHubDemo/src/com/example/GitHUbDemoMain.java
@@ -5,6 +5,11 @@ public class GitHUbDemoMain {
 	public static void main(String[] args) {
 		
 		Persona persona1 = null;
+		
+		Persona persona2 = null;
+		persona2 = new Persona();
+		
+		Genero genero;
 	}
 
 }
