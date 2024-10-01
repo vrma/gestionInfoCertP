9cb73fd7d71fa7609ef75b04bc47708838d5b95e
vrma <vrmachado@gmail.com>
2024-10-01 17:37:28 +0200

    modificada clase Persona para mas conflictos

--- GitHubDemo/src/com/example/Persona.java
+++ GitHubDemo/src/com/example/Persona.java
@@ -1,5 +1,62 @@
 package com.example;
 
-public class Persona {
+import java.time.LocalDate;
+import java.util.Objects;
 
+public class Persona {
+	
+	private LocalDate fechaNacimiento;
+	private double salario;
+	
+	public Persona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(LocalDate fechaNacimiento, double salario) {
+		super();
+		this.fechaNacimiento = fechaNacimiento;
+		this.salario = salario;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [fechaNacimiento=" + fechaNacimiento + ", salario=" + salario + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fechaNacimiento, salario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(fechaNacimiento, other.fechaNacimiento)
+				&& Double.doubleToLongBits(salario) == Double.doubleToLongBits(other.salario);
+	}
+	
+	
 }
