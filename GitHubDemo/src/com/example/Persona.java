1b2cfcbedbd987805db0e04575224a74fd61e627
rubenkiworks <rubengomezkiworks@gmail.com>
2024-10-01 17:37:21 +0200

    clase persona modificada para conflictos

--- GitHubDemo/src/com/example/Persona.java
+++ GitHubDemo/src/com/example/Persona.java
@@ -1,5 +1,61 @@
 package com.example;
 
+import java.util.Objects;
+
 public class Persona {
+	
+	private String nombre;
+	private Genero genero;
+	
+	public Persona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(String nombre, Genero genero) {
+		super();
+		this.nombre = nombre;
+		this.genero = genero;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(genero, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return genero == other.genero && Objects.equals(nombre, other.nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", genero=" + genero + "]";
+	}
+	
+	
 
 }
